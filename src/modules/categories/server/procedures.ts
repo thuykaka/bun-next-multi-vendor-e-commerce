a22a3190f5c651e7f7c324cce0a404e641077d52
ThuyLe <thuylv.work@gmail.com>
2025-07-15 21:31:31 +0700

    chap7: setup tRPC

---
+++ src/modules/categories/server/procedures.ts
@@ -0,0 +1,20 @@
+import { TRPCError } from '@trpc/server';
+import { baseProcedure, createTRPCRouter } from '@/trpc/init';
+
+export const categoriesRouter = createTRPCRouter({
+  getMany: baseProcedure.query(async ({ ctx }) => {
+    const categories = await ctx.payloadcms.find({
+      collection: 'categories',
+      depth: 1,
+      where: {
+        parent: {
+          exists: false
+        }
+      },
+      sort: 'name',
+      pagination: false
+    });
+
+    return categories;
+  })
+});
--- src/modules/home/ui/components/search-filters/index.tsx
+++ src/modules/home/ui/components/search-filters/index.tsx
@@ -1,17 +1,17 @@
-import { Category } from '@/payload-types';
-import { PaginatedDocs } from 'payload';
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 import Categories from './categories';
 import SearchInput from './search-input';
 
-type SearchFiltersProps = {
-  categories: PaginatedDocs<Category>;
-};
-
-export default function SearchFilters({ categories }: SearchFiltersProps) {
+export default function SearchFilters() {
   return (
     <div className='bg-background flex w-full flex-col items-start gap-4 border-b p-4 px-4 lg:px-8'>
       <SearchInput />
-      <Categories data={categories} />
+      <Suspense fallback={<div>Loading categories...</div>}>
+        <ErrorBoundary fallback={<div>Error loading categories...</div>}>
+          <Categories />
+        </ErrorBoundary>
+      </Suspense>
     </div>
   );
 }
